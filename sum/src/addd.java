5e8cfab99b1c366f9931d11ee4c08dafb5b0a573
unknown <adarshstha2016@gmail.com>
2016-05-24 17:49:40 +0545

    hello

---
+++ sum/src/addd.java
@@ -0,0 +1,17 @@
+
+
+public class addd {
+    public static void main (String[] args){
+      int sum = 0;
+        for (int i=2;i<=2;i++){
+            for(int j=1;j<=10;j=j+2){
+                System.out.println(i*j);
+                sum=sum+(i*j);
+        
+    }
+
+    }
+         System.out.println("sum ="+ sum);
+    
+}
+}
